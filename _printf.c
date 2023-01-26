ccb55adcccb7ef5995c281a7b10d1e1b6dd85d7b
LaChacal <you@example.com>
2023-01-26 11:08:22 -0800

    produces output according to format

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 
-	int flags, width, precision, size, buff_ind = 0;
+	int buff_ind = 0;
 
 	va_list list;
 
@@ -22,7 +22,6 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
@@ -36,14 +35,8 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+
 			if (printed == -1)
 				return (-1);
 
@@ -52,9 +45,9 @@ int _printf(const char *format, ...)
 	}
 	print_buffer(buffer, &buff_ind);
 	va_end(list);
+
 	return (printed_chars);
 }
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
